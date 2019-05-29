c0f9967db27360f62807ecbdf671f664caae0890
satoru.narisawa <hard.to.say.im.sorry08@gmail.com>
2019-05-29 04:37:15 +0000

    user login

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
-Route::get("/","TasksController@index");
+Route::get("/",function(){
+    return view("welcome");
+});
 
 Route::resource("tasks","TasksController");
+
+//アカウント登録機能
+Route::get("signup","Auth\RegisterController@showRegistrationForm")->name("signup.get");
+Route::post("signup","Auth\RegisterController@register")->name("signup.post");
+
+//ログイン機能
+Route::get("login","Auth\LoginController@showLoginForm")->name("login");
+Route::post("Login","Auth\LoginController@Login")->name("login.post");
+Route::get("logout","Auth\LoginController@logout")->name("logout.get");
